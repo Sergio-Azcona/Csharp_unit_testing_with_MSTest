e1a09f018e887fdde1b777eefd2c326d64474eef
Sergio <sergioazcona@odysseylogistics.com>
2023-10-03 17:10:51 -0400

    tests updates

--- PrimeService_IsPrimeShould.cs
+++ PrimeService_IsPrimeShould.cs
@@ -17,73 +17,76 @@ namespace Prime.UnitTests.Services
         }
 
         [TestMethod]
-        public void IsPrime_InputIsneg1_ReturnFalse()
+        public void IsPrime_InputIsNotPrime_ReturnFalse()
         {
-            var result = _primeService.IsPrime(-1);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result, "1 should not be prime");
-        } 
-
-        [TestMethod]
-        public void IsPrime_InputIs1_ReturnFalse()
-        {
-            var result = _primeService.IsPrime(1);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result, "1 should not be prime");
-        } 
-
-        [TestMethod]
-        public void IsPrime_InputIs2_Returntrue()
-        {
-            var result = _primeService.IsPrime(2);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result, "2 is a prime number div by 2");
-        }
-
-        [TestMethod]
-        public void IsPrime_InputIs9_Returntrue()
-        {
-            var result = _primeService.IsPrime(9);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result, "9 is NOT a prime number");
-        }
-
-        [TestMethod]
-        public void IsPrime_InputIs21_Returntrue()
-        {
-            var result = _primeService.IsPrime(21);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result, "21 is NOT a prime number div by 2");
-        }
-
-        [TestMethod]
-        public void IsPrime_InputIs23_Returntrue()
-        {
-            var result = _primeService.IsPrime(23);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result, "23 is a prime number");
-        }
-
-        [TestMethod]
-        public void IsPrime_InputIs31_Returntrue()
-        {
-            var result = _primeService.IsPrime(31);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result, "31 is a prime number");
-        }
-
-        [TestMethod]
-        public void IsPrime_InputIs561_Returntrue()
-        {
-            var result = _primeService.IsPrime(561);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result, "561 is NOT a prime number");
-        }
-
-        [TestMethod]
-        public void IsPrime_InputIs41041_Returntrue()
-        {
-            var result = _primeService.IsPrime(41041);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result, "41041 is NOT a prime number");
-        }
+            var resultNeg10 = _primeService.IsPrime(-10);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(resultNeg10, "-10 should not be prime");
         
+            var result1 = _primeService.IsPrime(1);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result1, "1 should not be prime");
+
+            var result9 = _primeService.IsPrime(9);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result9, "9 is NOT a prime number");
+        
+            var result21 = _primeService.IsPrime(21);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result21, "21 is NOT a prime number div by 2");
+
+            var result561 = _primeService.IsPrime(561);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result561, "561 is NOT a prime number");
+
+            var result41041 = _primeService.IsPrime(41041);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsFalse(result41041, "41041 is NOT a prime number");        
+        } 
+
         [TestMethod]
-        public void IsPrime_InputIs23209_Returntrue()
+        public void IsPrime_InputIsPrime_ReturnTrue()
         {
-            var result = _primeService.IsPrime(23209);
-            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result, "23209 is a prime number");
+            var result2 = _primeService.IsPrime(2);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result2, "2 is a prime number div by 2");
+
+            var result23 = _primeService.IsPrime(23);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result23, "23 is a prime number");
+
+            var result31 = _primeService.IsPrime(31);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result31, "31 is a prime number");
+
+            var result569 = _primeService.IsPrime(569);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result569, "569 is a prime number");
+        
+            var result23209 = _primeService.IsPrime(23209);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsTrue(result23209, "23209 is a prime number");
+        }
+
+        [TestMethod]
+        public void PrimeStatement_InputIsNotPrime_ReturnNegationString()
+        {
+            int number1 = 1;
+            string result1 = _primeService.PrimeStatement(number1);
+            Assert.AreEqual($"{number1} is not a prime number.", result1);
+
+            int number21 = 21;
+            string resultPS21 = _primeService.PrimeStatement(number21);
+            Assert.AreEqual($"{number21} is not a prime number.", resultPS21);
+            
+            int number561 = 561;
+            string result561 = _primeService.PrimeStatement(number561);
+            Assert.AreNotEqual($"{number561} is, in fact, a prime number.", result561);
+        }
+
+        [TestMethod]
+        public void PrimeStatement_InputIsPrime_ReturnAffirmativeString()
+        {
+            int number23 = 23;
+            string result23 = _primeService.PrimeStatement(number23);
+            Assert.AreNotEqual($"{number23} is not a prime number.", result23);
+
+            int number811 = 811;
+            string resultPS811 = _primeService.PrimeStatement(number811);
+            Assert.AreEqual($"{number811} is, in fact, a prime number.", resultPS811);
+            
+            int number224737 = 224737;
+            string result224737 = _primeService.PrimeStatement(number224737);
+            Assert.AreEqual($"{number224737} is, in fact, a prime number.", result224737);
         }
     }
 }
---
+++ QuickCalculatorTest.cs
@@ -0,0 +1,27 @@
+﻿// https://learn.microsoft.com/en-us/training/modules/visual-studio-test-tools/2-create-test
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using unit_testing_using_mstest;
+
+namespace PrimeService.Tests
+{
+    public class QuickCalculatorTest
+    {
+
+        public void SetCalcualtor()
+        {
+            QuickCalculator calculator = new QuickCalculator();
+        }
+
+        [TestMethod]
+
+        public void QuickAdd_SimpleTest() 
+        {
+        
+        }
+    }
+}
