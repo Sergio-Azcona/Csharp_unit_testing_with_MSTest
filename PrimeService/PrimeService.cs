97d4a1b6760d3a4a1b5cebf6134d76e22cf2faba
Sergio <sergioazcona@odysseylogistics.com>
2023-10-03 16:58:42 -0400

    added new class to test (QuickCalcualtor) and test file

---
+++ PrimeService/PrimeService.cs
@@ -0,0 +1,47 @@
+﻿// https://learn.microsoft.com/en-us/dotnet/core/testing/unit-testing-with-nunit?source=recommendations
+using System;
+
+namespace Prime.Services
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            if (candidate <= 1) 
+            {
+                return false;
+            }
+            else if (candidate == 2 || candidate == 3)
+            {
+                return true;
+            }
+            else if (candidate % 2 == 0 || candidate % 3 == 0)
+            {
+                return false;
+            }
+            else if (candidate >= 5)
+            {
+                // 6K +/- 1
+                for (int i = 5; i * i <= candidate; i += 6)
+                    {
+                        if (candidate % i == 0 || candidate % (i + 2) == 0)
+                        {
+                            return false;
+                        }
+                    }
+            }
+
+            return true;
+        }
+
+        public string PrimeStatement(int number) 
+        {
+            if (IsPrime(number) == true)
+            {
+                return($"{number} is, in fact, a prime number.");
+            }
+
+            return($"{number} is not a prime number.");
+        }
+    }
+}
---
+++ PrimeService/QuickCalculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace unit_testing_using_mstest
+{
+    public class QuickCalculator
+    {
+        public int QuickAdd(int num1, int num2) 
+        {
+            return num1 + num2;
+        }
+
+        public int QuickSubtract(int num1, int num2) 
+        {
+            return num1 - num2; 
+        }
+
+        public bool QuickSumDivisibleBy2(int num1, int num2) 
+        {
+            if( (num1 + num2) % 2 == 0) 
+            { 
+                return true;    
+            }
+
+            return false;
+        }
+    }
+}
